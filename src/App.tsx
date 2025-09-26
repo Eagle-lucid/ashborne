bfaa71bb50f5979370c6eefae293dac438808f90
Eagle-lucid <vicokefie@gmail.com>
2025-09-25 22:58:12 -0400

    feat(app): setup main App component with Home page
    - Created App component as the root container
    - Rendered <Home /> inside a wrapper div
    - Exported App as default for entry point usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+// App.tsx
+import { Home } from "./pages/Home";
+
 function App() {
-  
+  return (
+    <div className="App">
+      <Home />
+    </div>
+
+  );
 }
 
-export default App
+export default App;
