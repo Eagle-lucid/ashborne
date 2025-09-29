deacefb10a736cf1cef23a219a47f425c0b8b69b
Eagle-lucid <vicokefie@gmail.com>
2025-09-29 10:48:29 -0400

    chore(hero): add barrel export file for Hero section components

---
+++ src/sections/Hero/index.ts
@@ -0,0 +1,6 @@
+// src/sections/Hero/index.ts {Export Barrel}
+export { Hero } from './Hero';
+export type { HeroProps } from './Hero';
+export { HeroContent } from './HeroContent';
+export { HeroMockup } from './HeroMockup';
+export { HeroShapes } from './HeroShapes';
